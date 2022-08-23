c0952c555df5ccdab3aa6ebbab963c7f46901759
Oleksandr <alexkrekoten11@gmail.com>
2022-08-23 08:49:41 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+    startChangeColorBtn: document.querySelector('[data-start]'),
+    stopChangeColorBtn: document.querySelector('[data-stop]'),
+    body: document.querySelector('body'),
+    spinner: document.querySelector('.spinner-grow'),
+}
+const DELAY = 1000;
+let intervalID = null; 
+refs.startChangeColorBtn.addEventListener('click', onChangeColor)
+refs.stopChangeColorBtn.addEventListener('click', stopChangeColor)
+refs.stopChangeColorBtn.disabled = true
+
+
+function onChangeColor(event) {
+    refs.stopChangeColorBtn.disabled = false
+    refs.startChangeColorBtn.disabled = true
+        refs.spinner.classList.remove('d-none') 
+        console.log(1);
+    intervalID = setInterval(() => {
+        refs.body.style.background = `linear-gradient(to right, ${getRandomHexColor()} 0%, ${getRandomHexColor()} 100%)`
+    }, DELAY)
+    
+}
+function stopChangeColor() {
+    clearInterval(intervalID)
+ refs.stopChangeColorBtn.disabled = true
+    refs.startChangeColorBtn.disabled = false
+    refs.spinner.classList.add('d-none')
+    
+}
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
